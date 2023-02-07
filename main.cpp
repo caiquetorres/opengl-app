b5486495bcd7da6baed37dd8bb6e7aef066cc0f0
Caique Torres <caiquet.bastos@gmail.com>
2023-02-06 22:19:59 -0300

    feat(main.cpp & basic.glsl): rendering some squares

--- main.cpp
+++ main.cpp
@@ -27,14 +27,47 @@ int main()
 
   graphics::shader basicShader("shaders/basic.glsl");
 
+  glm::vec3 positions[] = {
+      glm::vec3(0.0f, 0.0f, 0.0f),
+      glm::vec3(2.0f, 5.0f, -15.0f),
+      glm::vec3(-1.5f, -2.2f, -2.5f),
+      glm::vec3(-3.8f, -2.0f, -12.3f),
+      glm::vec3(2.4f, -0.4f, -3.5f),
+      glm::vec3(-1.7f, 3.0f, -7.5f),
+      glm::vec3(1.3f, -2.0f, -2.5f),
+      glm::vec3(1.5f, 2.0f, -2.5f),
+      glm::vec3(1.5f, 0.2f, -1.5f),
+      glm::vec3(-1.3f, 1.0f, -1.5f)};
+
   while (window.is_open())
   {
     window.clear();
 
-    basicShader.use();
     va.bind();
+    basicShader.use();
 
-    glDrawElements(GL_TRIANGLES, (int)vertices.size(), GL_UNSIGNED_INT, nullptr);
+    const float radius = 10.0f;
+    float camX = sin(glfwGetTime()) * radius;
+    float camZ = cos(glfwGetTime()) * radius;
+
+    glm::mat4 view;
+    view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, -3.0), glm::vec3(0.0, 1.0, 0.0));
+
+    glm::mat4 projection;
+    projection = glm::perspective(glm::radians(45.0f), (float)window.get_width() / window.get_height(), 0.1f, 100.0f);
+
+    basicShader.set_mat4("view", view);
+    basicShader.set_mat4("projection", projection);
+
+    for (unsigned int i = 0; i < 10; i++)
+    {
+      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::translate(model, positions[i]);
+      model = glm::rotate(model, glm::radians(20.0f * i), glm::vec3(1.0f, 0.3f, 0.5f));
+      basicShader.set_mat4("model", model);
+
+      glDrawElements(GL_TRIANGLES, (int)vertices.size(), GL_UNSIGNED_INT, nullptr);
+    }
 
     window.update();
   }
--- res/shaders/basic.glsl
+++ res/shaders/basic.glsl
@@ -4,9 +4,13 @@
 
 layout (location = 0) in vec3 position;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-    gl_Position = vec4(position.x, position.y, position.z, 1.0);
+    gl_Position = projection * view * model * vec4(position.x, position.y, position.z, 1.0);
 }
 
 #endif
@@ -17,7 +21,7 @@ layout (location = 0) out vec4 color;
 
 void main()
 {
-    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    color = vec4(1.0f, 0.5f, 0.3f, 1.0f);
 }
 
 #endif
