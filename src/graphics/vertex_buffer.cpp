427e8581a69fabf2c350c6e1c7273b278983bc68
Caique Torres <caiquet.bastos@gmail.com>
2023-02-08 21:11:05 -0300

    feat(vertex_buffer.h & vertex_buffer.cpp): removed some unused constructor parameters; removed some unnecessary code

--- src/graphics/vertex_buffer.cpp
+++ src/graphics/vertex_buffer.cpp
@@ -1,6 +1,6 @@
 #include "vertex_buffer.h"
 
-graphics::vertex_buffer::vertex_buffer(std::vector<float> &vertices, int vertex_size, unsigned int draw_type)
+graphics::vertex_buffer::vertex_buffer(std::vector<float> &vertices, unsigned int draw_type)
     : m_Id(0),
       m_Vertices(vertices)
 {
@@ -10,18 +10,6 @@ graphics::vertex_buffer::vertex_buffer(std::vector<float> &vertices, int vertex_
   // The array size in bytes.
   unsigned long int size = sizeof(float) * vertices.size();
   glCheckError(glBufferData(GL_ARRAY_BUFFER, size, &vertices[0], draw_type));
-
-  // The vertex size in bytes.
-  int stride = (int)(8 * sizeof(float));
-
-  // TODO: Move this statements into vertex_array
-  glCheckError(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, nullptr));
-  glCheckError(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, nullptr));
-  glCheckError(glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, nullptr));
-
-  glCheckError(glEnableVertexAttribArray(0));
-  glCheckError(glEnableVertexAttribArray(1));
-  glCheckError(glEnableVertexAttribArray(2));
 }
 
 unsigned int graphics::vertex_buffer::get_id() const
--- src/graphics/vertex_buffer.h
+++ src/graphics/vertex_buffer.h
@@ -23,10 +23,9 @@ namespace graphics
   public:
     /**
      * @param vertices the vertices set.
-     * @param vertex_size the vertex element size.
      * @param draw_type the frequency which the array will be accessed.
      */
-    vertex_buffer(std::vector<float> &vertices, int vertex_size, unsigned int draw_type);
+    vertex_buffer(std::vector<float> &vertices, unsigned int draw_type);
 
     /**
      * @brief Gets the vertex buffer object identifier.
