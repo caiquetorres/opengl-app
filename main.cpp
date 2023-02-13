cb24b272d4a99cb98fe87c887c12f1cfa134de3b
Caique Torres <caiquet.bastos@gmail.com>
2023-02-13 12:51:05 -0300

    feat(main.cpp): implemented a simple transformation for testing purposes using imgui

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <memory>
+#include <cmath>
 
 #include "src/graphics/window.h"
 #include "src/graphics/vertex.h"
@@ -43,45 +44,46 @@ int main()
 
   graphics::mesh mesh(vertices, indices, basicTexture);
 
-  glm::vec3 positions[] = {
-      glm::vec3(0.0f, 0.0f, 0.0f),
-      glm::vec3(2.0f, 5.0f, -15.0f),
-      glm::vec3(-1.5f, -2.2f, -2.5f),
-      glm::vec3(-3.8f, -2.0f, -12.3f),
-      glm::vec3(2.4f, -0.4f, -3.5f),
-      glm::vec3(-1.7f, 3.0f, -7.5f),
-      glm::vec3(1.3f, -2.0f, -2.5f),
-      glm::vec3(1.5f, 2.0f, -2.5f),
-      glm::vec3(1.5f, 0.2f, -1.5f),
-      glm::vec3(-1.3f, 1.0f, -1.5f)};
+  float rotation[] = {0.0, 0.0, 0.0};
+  float position[] = {0.0, 0.0, 0.0};
 
   while (window.is_open())
   {
     window.clear();
 
-    const float radius = 10.0f;
-    float camX = sin(glfwGetTime()) * radius;
-    float camZ = cos(glfwGetTime()) * radius;
-
     glm::mat4 view;
-    view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, -3.0), glm::vec3(0.0, 1.0, 0.0));
+    view = glm::lookAt(glm::vec3(3, 0.0, 3), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 10.0, 0.0));
 
     glm::mat4 projection;
     projection = glm::perspective(glm::radians(45.0f), (float)window.get_width() / window.get_height(), 0.1f, 100.0f);
 
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(position[0], position[1], position[2]));
+
+    model = glm::rotate(model, glm::radians(rotation[0]), glm::vec3(1.0f, 0.0f, 0.0f));
+    model = glm::rotate(model, glm::radians(rotation[1]), glm::vec3(0.0f, 1.0f, 0.0f));
+    model = glm::rotate(model, glm::radians(rotation[2]), glm::vec3(0.0f, 0.0f, 1.0f));
+
     basicShader.set_mat4("view", view);
     basicShader.set_mat4("projection", projection);
+    basicShader.set_mat4("model", model);
 
-    for (int i = 0; i < 10; i++)
-    {
-      glm::mat4 model = glm::mat4(1.0f);
-      model = glm::translate(model, positions[i]);
-      model = glm::rotate(model, glm::radians(20.0f * i), glm::vec3(1.0f, 0.3f, 0.5f));
+    mesh.draw(basicShader);
 
-      basicShader.set_mat4("model", model);
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
 
-      mesh.draw(basicShader);
-    }
+    ImGui::NewFrame();
+
+    ImGui::Begin("Tool");
+
+    ImGui::SliderFloat3("rotation", rotation, 0, 360);
+    ImGui::DragFloat3("position", position, 0);
+
+    ImGui::End();
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
     window.update();
   }
